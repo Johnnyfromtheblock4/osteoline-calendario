f1e5aa4dd02842672c1701320bbd8c2041a59eed
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-11-03 13:38:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from "./components/CalendarApp";
+import "./components/CalendarApp";
+
+const App = () => {
+  return (
+    <>
+      <CalendarApp />
+    </>
+  );
+};
+
+export default App;
